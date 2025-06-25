b07057015089f49f58853e6018c65b55d21c2cbf
Eo-Jin Nam (Jake) <121080902+JakeANam@users.noreply.github.com>
2025-06-25 12:49:39 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss'
+import Payment from './components/Payment';
+
+function App() {
+  return (
+    <div className="App">
+      <Payment></Payment>
+    </div>
+  );
+}
+
+export default App;
