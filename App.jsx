74e5aedece836398e8c5b2d1a7df3838156af346
Eo-Jin Nam (Jake) <121080902+JakeANam@users.noreply.github.com>
2025-06-27 15:36:33 +0900

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,11 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/App.scss'
 import Payment from './components/Payment';
-
+import Paymentdetail from './components/Paymentdetail'
+import Usage from './components/Usage'
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Payment></Payment>
+      <Routes>
+        <Route path='/a' element={<Payment/>}></Route>
+        <Route path='/b' element={<Paymentdetail/>}></Route>
+        <Route path='/c' element={<Usage/>}></Route>
+      </Routes>
     </div>
   );
 }
